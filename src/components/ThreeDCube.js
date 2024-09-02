51c6b07978373a3f4728ab4ae6d79c2fc2c0a4cf
NjoguRufus <rsgxliv@gmail.com>
2024-09-02 21:37:25 +0300

    ThreeD

--- src/components/ThreeDCube.js
+++ src/components/ThreeDCube.js
@@ -1,17 +1,30 @@
 // src/components/ThreeDCube.js
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
+
+const RotatingCube = () => {
+  const meshRef = useRef();
+  
+  useFrame(() => {
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color={'blue'} />
+    </mesh>
+  );
+};
 
 const ThreeDCube = () => {
   return (
     <Canvas className="h-64">
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <mesh rotation={[45, 0, 0]}>
-        <boxGeometry args={[2, 2, 2]} />
-        <meshStandardMaterial color={'blue'} />
-      </mesh>
+      <RotatingCube />
       <OrbitControls />
     </Canvas>
   );
