2d5aaadeb67d96bee58f9285c26bc75e6339fbaf
NjoguRufus <rsgxliv@gmail.com>
2024-09-02 22:11:39 +0300

    ThreeD2

--- src/components/ThreeDCube.js
+++ src/components/ThreeDCube.js
@@ -1,10 +1,11 @@
-// src/components/ThreeDCube.js
 import React, { useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useTexture } from '@react-three/drei';
+import { EffectComposer, Bloom } from '@react-three/postprocessing';
 
 const RotatingCube = () => {
   const meshRef = useRef();
+  const texture = useTexture('/path/to/your/texture.jpg');
   
   useFrame(() => {
     meshRef.current.rotation.x += 0.01;
@@ -14,7 +15,7 @@ const RotatingCube = () => {
   return (
     <mesh ref={meshRef}>
       <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color={'blue'} />
+      <meshStandardMaterial map={texture} />
     </mesh>
   );
 };
@@ -22,10 +23,14 @@ const RotatingCube = () => {
 const ThreeDCube = () => {
   return (
     <Canvas className="h-64">
-      <ambientLight />
+      <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
+      <spotLight position={[-5, 5, 10]} angle={0.3} penumbra={1} intensity={1} castShadow />
       <RotatingCube />
       <OrbitControls />
+      <EffectComposer>
+        <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} />
+      </EffectComposer>
     </Canvas>
   );
 };
